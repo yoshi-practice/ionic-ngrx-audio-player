a6a30a12d7f2152400a8e94faa86bb47bed49087
y-kaino <yoppy1125yoppy@gmail.com>
2019-01-15 16:29:56 +0900

    [Update] audio provider

--- src/providers/audio/audio.ts
+++ src/providers/audio/audio.ts
@@ -1,17 +1,74 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
+import * as moment from 'moment';
 
-/*
-  Generated class for the AudioProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class AudioProvider {
+  private stop$ = new Subject();
+  private audioObj = new Audio();
 
-  constructor(public http: HttpClient) {
-    console.log('Hello AudioProvider Provider');
+  constructor() { }
+
+  private streamObservable(url) {
+    let events = [
+      'ended', 'error', 'play', 'playing', 'pause', 'timeupdate', 'canplay', 'loadedmetadata', 'loadstart'
+    ];
+
+    const addEvents = function(obj, events, handler) {
+      events.forEach(event => {
+        obj.addEventListener(event, handler);
+      });
+    };
+
+    const removeEvents = function(obj, events, handler) {
+      events.forEach(event => {
+        obj.removeEventListener(event, handler);
+      });
+    };
+
+    return Observable.create(observer => {
+      // 再生オーディオ
+      this.audioObj.src = url;
+      this.audioObj.load();
+      this.audioObj.play();
+
+      // メディアイベント
+      const handler = (event) => observer.next(event);
+      addEvents(this.audioObj, events, handler);
+
+      return () => {
+        // 再生停止
+        this.audioObj.pause();
+        this.audioObj.currentTime = 0;
+
+        // EventListeners を削除します
+        removeEvents(this.audioObj, events, handler);
+      };
+    });
   }
 
+  play() {
+    this.audioObj.play();
+  }
+
+  pause() {
+    this.audioObj.pause();
+  }
+
+  stop() {
+    this.stop$.next();
+  }
+
+  seekTo(seconds) {
+    this.audioObj.currentTime = seconds;
+  }
+
+  formatTime(time, format) {
+    return moment.utc(time).format(format);
+  }
+  
+  playStream(url) {
+    return this.streamObservable(url).pipe(takeUntil(this.stop$));
+  }
 }
